2115125d24f06467bba114ff506d7293f7cb71a1
David Zavalla <davidzavalla@gmail.com>
2020-04-25 20:52:11 -0300

    Show help when there's not enough arguments

--- index.js
+++ index.js
@@ -1,28 +1,32 @@
 const parser = require('cue-parser');
 
-const cuesheet = parser.parse(process.argv[2]);
-
-cuesheet.files[0].tracks.forEach((item) => {
-    let hour;
-    let minute;
-    let second;
-    if (item.indexes[0].time.min >= 60) {
-        hour = Math.floor(item.indexes[0].time.min/60);
-        minute = item.indexes[0].time.min - 60;
-    } else {
-        hour = 0;
-        minute = item.indexes[0].time.min 
-    }
-    if (minute < 10) {
-        minute = `0${minute}`;
-    }
-    if (item.indexes[0].time.sec < 10) {
-        second = `0${item.indexes[0].time.sec}`;
-    } else {
-        second = item.indexes[0].time.sec;
-    }
-    if (hour < 10) {
-        hour = `0${hour}`;
-    }
-    console.log(`${hour}:${minute}:${second} ${item.performer} - ${item.title}`);
-});
+if (process.argv.length < 3) {
+    console.log(`Usage: node index.js <path to cue sheet file>`);
+} else {
+    const cuesheet = parser.parse(process.argv[2]);
+    
+    cuesheet.files[0].tracks.forEach((item) => {
+        let hour;
+        let minute;
+        let second;
+        if (item.indexes[0].time.min >= 60) {
+            hour = Math.floor(item.indexes[0].time.min/60);
+            minute = item.indexes[0].time.min - 60;
+        } else {
+            hour = 0;
+            minute = item.indexes[0].time.min 
+        }
+        if (minute < 10) {
+            minute = `0${minute}`;
+        }
+        if (item.indexes[0].time.sec < 10) {
+            second = `0${item.indexes[0].time.sec}`;
+        } else {
+            second = item.indexes[0].time.sec;
+        }
+        if (hour < 10) {
+            hour = `0${hour}`;
+        }
+        console.log(`${hour}:${minute}:${second} ${item.performer} - ${item.title}`);
+    });
+}
